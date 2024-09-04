9d793fc184d75c7758d327d77ee94493e7f9a9f8
daeun <sss3598@gmail.com>
2024-09-04 21:59:16 +0900

    오브젝트와 의존관계 | 스프링 컨테이너 의존성 주입(BeanFactory)

--- src/main/java/org/example/hellospring/ObjectFactory.java
+++ src/main/java/org/example/hellospring/ObjectFactory.java
@@ -1,11 +1,17 @@
 package org.example.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
 
+  @Bean
   public PaymentService paymentService() {
     return new PaymentService(exRateProvider());
   }
 
+  @Bean
   public ExRateProvider exRateProvider() {
     return new WebApiExRateProvider();
   }
