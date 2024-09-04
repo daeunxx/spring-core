9cc171c95baf0504f8fd900822bdff4f60de04c3
daeun <sss3598@gmail.com>
2024-09-04 21:12:52 +0900

    오브젝트와 의존관계 | 2가지 관심사(Provider, 값 설정) 각각 분리

---
+++ src/main/java/org/example/hellospring/ObjectFactory.java
@@ -0,0 +1,12 @@
+package org.example.hellospring;
+
+public class ObjectFactory {
+
+  public PaymentService paymentService() {
+    return new PaymentService(exRateProvider());
+  }
+
+  public ExRateProvider exRateProvider() {
+    return new WebApiExRateProvider();
+  }
+}
