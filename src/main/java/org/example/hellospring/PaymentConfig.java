1c0199b41568922b31a7b713cffd99fcd65024e0
daeun <sss3598@gmail.com>
2024-09-07 16:57:01 +0900

    템플릿 | RestTemplate 적용

--- src/main/java/org/example/hellospring/PaymentConfig.java
+++ src/main/java/org/example/hellospring/PaymentConfig.java
@@ -5,12 +5,14 @@ import org.example.hellospring.api.ApiTemplate;
 import org.example.hellospring.api.ErApiExRateExtractor;
 import org.example.hellospring.api.SimpleApiExecutor;
 import org.example.hellospring.exrate.CachedExRateProvider;
+import org.example.hellospring.exrate.RestTemplateExRateProvider;
 import org.example.hellospring.payment.ExRateProvider;
-import org.example.hellospring.exrate.WebApiExRateProvider;
 import org.example.hellospring.payment.PaymentService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @ComponentScan
@@ -29,7 +31,13 @@ public class PaymentConfig {
 
   @Bean
   public ExRateProvider exRateProvider() {
-    return new WebApiExRateProvider(apiTemplate());
+    //return new WebApiExRateProvider(apiTemplate());
+    return new RestTemplateExRateProvider(restTemplate());
+  }
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate(new JdkClientHttpRequestFactory());
   }
 
   @Bean
