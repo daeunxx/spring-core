b6cdcb20774dc6e6ba506f089114f9388870f41a
daeun <sss3598@gmail.com>
2024-10-02 21:10:17 +0900

    스프링 기본편 프로젝트 생성

---
+++ src/test/java/core/springbasic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package core.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
