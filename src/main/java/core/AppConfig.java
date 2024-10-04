4ea15091d3ac79d0a9577cbe88c5c47a6f0f55b5
daeun <sss3598@gmail.com>
2024-10-04 20:30:40 +0900

    객체 지향 | Configuration 리팩토링

--- src/main/java/core/AppConfig.java
+++ src/main/java/core/AppConfig.java
@@ -1,6 +1,8 @@
 package core;
 
+import core.springbasic.discount.DiscountPolicy;
 import core.springbasic.discount.FixDiscountPolicy;
+import core.springbasic.member.MemberRepository;
 import core.springbasic.member.MemberService;
 import core.springbasic.member.MemberServiceImpl;
 import core.springbasic.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import core.springbasic.order.OrderServiceImpl;
 public class AppConfig {
 
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
+  }
+
+  private MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  private DiscountPolicy discountPolicy() {
+    return new FixDiscountPolicy();
   }
 
 }
