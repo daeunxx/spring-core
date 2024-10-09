b04aa0bfade8c7904805738c5b2c0ddfbe7f090e
daeun <sss3598@gmail.com>
2024-10-09 17:02:39 +0900

    빈 생명주기 콜백 | InitializingBean, DisposableBean

---
+++ src/test/java/core/springbasic/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package core.springbasic.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+  @Test
+  public void lifeCycleTest() {
+    ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+    NetworkClient networkClient = context.getBean(NetworkClient.class);
+    context.close();
+  }
+
+  @Configuration
+  static class LifeCycleConfig {
+
+    @Bean
+    public NetworkClient networkClient() {
+      NetworkClient networkClient = new NetworkClient();
+      networkClient.setUrl("http://hello-spring.dev");
+      return networkClient;
+    }
+
+  }
+}
---
+++ src/test/java/core/springbasic/lifecycle/NetworkClient.java
@@ -0,0 +1,44 @@
+package core.springbasic.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+  private String url;
+
+  public NetworkClient() {
+    System.out.println("생성자 호출, url = " + url);
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    System.out.println("NetworkClient.afterPropertiesSet");
+    connect();
+    call("초기화 연결 메시지");
+  }
+
+  @Override
+  public void destroy() throws Exception {
+    System.out.println("NetworkClient.destroy");
+    disconnect();
+  }
+
+  // 서비스 시작 시, 호출
+  public void connect() {
+    System.out.println("connect: " + url);
+  }
+
+  public void call(String message) {
+    System.out.println("call: " + url + ", massage = " + message);
+  }
+
+  // 서비스 종료 시, 호출
+  public void disconnect() {
+    System.out.println("disconnect: " + url);
+  }
+}
