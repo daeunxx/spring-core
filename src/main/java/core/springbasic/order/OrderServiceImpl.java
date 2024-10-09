cac9711430c0b035fa352a92ff9456eae6e546f4
daeun <sss3598@gmail.com>
2024-10-09 10:59:26 +0900

    의존 관계 자동 주입 | @RequiredArgsConstructor

--- src/main/java/core/springbasic/order/OrderServiceImpl.java
+++ src/main/java/core/springbasic/order/OrderServiceImpl.java
@@ -3,18 +3,15 @@ package core.springbasic.order;
 import core.springbasic.discount.DiscountPolicy;
 import core.springbasic.member.Member;
 import core.springbasic.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
-
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    this.memberRepository = memberRepository;
-    this.discountPolicy = discountPolicy;
-  }
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
